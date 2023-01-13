91e26e09a6098014c294a73b2df4e43445149b22
Görkem Aydoğdu <gorkem_aydogdu15@hotmail.com>
2023-01-13 19:53:16 +0300

    Lemkus Clone

--- src/components/Wishlist/WishlistSelectedDetail.js
+++ src/components/Wishlist/WishlistSelectedDetail.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 import Button from "../UI/Button";
 import { ReactComponent as ArrowLeft } from "../../assets/keyboard_arrow_left.svg";
-import { ReactComponent as ArrowRight } from "../../assets/chevron-right.svg";
+// import { ReactComponent as ArrowRight } from "../../assets/chevron-right.svg";
 import { ReactComponent as ArrowDown } from "../../assets/triangle-down.svg";
 
 //Splidejs
@@ -18,12 +18,23 @@ const WishlistSelectedDetail = () => {
     });
 
     splide.on("pagination:mounted", function (data) {
-      // You can add your class to the UL element
+      data.list.classList.remove(
+        "splide__pagination",
+        "splide__pagination--ltr"
+      );
+
       data.list.classList.add("wishlist__thumbnail");
 
-      // `items` contains all dot items
       data.items.forEach(function (item) {
-        item.button.textContent = String(item.page + 1);
+        item.li.classList.add("wishlist__thumbnailItem");
+
+        item.button.classList.remove("splide__pagination__page");
+
+        item.button.classList.add("wishlist__thumbnailButton");
+
+        item.button.innerHTML = `<img src='https://cdn.shopify.com/s/files/1/0538/9280/8895/products/DM3384-600-${
+          item.page + 1
+        }.png?v=1665052479' alt='Product ${item.page + 1} of 7'/>`;
       });
     });
 
@@ -39,82 +50,6 @@ const WishlistSelectedDetail = () => {
       </Button>
       <div className="wishlist__selectedProduct">
         <div className="wishlist__images">
-          <ul
-            role="tablist"
-            aria-label="Select a slide to show"
-            className="wishlist__thumbnail"
-          >
-            <li role="presentation" className="wishlist__thumbnailItem">
-              <img
-                role="tab"
-                aria-controls="custom-pagination-slide01"
-                aria-label="Go to slide 1"
-                aria-selected="true"
-                src="https://cdn.shopify.com/s/files/1/0538/9280/8895/products/DM3384-600-1.png?v=1665052479"
-                alt="Product 1 of 7"
-              />
-            </li>
-            <li role="presentation" className="wishlist__thumbnailItem">
-              <img
-                role="tab"
-                aria-controls="custom-pagination-slide02"
-                aria-label="Go to slide 2"
-                tabIndex="-1"
-                src="https://cdn.shopify.com/s/files/1/0538/9280/8895/products/DM3384-600-2.png?v=1665052479"
-                alt="Product 2 of 7"
-              />
-            </li>
-            <li role="presentation" className="wishlist__thumbnailItem">
-              <img
-                role="tab"
-                aria-controls="custom-pagination-slide03"
-                aria-label="Go to slide 3"
-                tabIndex="-1"
-                src="https://cdn.shopify.com/s/files/1/0538/9280/8895/products/DM3384-600-3.png?v=1665052479"
-                alt="Product 3 of 7"
-              />
-            </li>
-            <li role="presentation" className="wishlist__thumbnailItem">
-              <img
-                role="tab"
-                aria-controls="custom-pagination-slide04"
-                aria-label="Go to slide 4"
-                tabIndex="-1"
-                src="https://cdn.shopify.com/s/files/1/0538/9280/8895/products/DM3384-600-4.png?v=1665052479"
-                alt="Product 4 of 7"
-              />
-            </li>
-            <li role="presentation" className="wishlist__thumbnailItem">
-              <img
-                role="tab"
-                aria-controls="custom-pagination-slide05"
-                aria-label="Go to slide 5"
-                tabIndex="-1"
-                src="https://cdn.shopify.com/s/files/1/0538/9280/8895/products/DM3384-600-5.png?v=1665052479"
-                alt="Product 5 of 7"
-              />
-            </li>
-            <li role="presentation" className="wishlist__thumbnailItem">
-              <img
-                role="tab"
-                aria-controls="custom-pagination-slide06"
-                aria-label="Go to slide 6"
-                tabIndex="-1"
-                src="https://cdn.shopify.com/s/files/1/0538/9280/8895/products/DM3384-600-6.png?v=1665052479"
-                alt="Product 6 of 7"
-              />
-            </li>
-            <li role="presentation" className="wishlist__thumbnailItem">
-              <img
-                role="tab"
-                aria-controls="custom-pagination-slide07"
-                aria-label="Go to slide 7"
-                tabIndex="-1"
-                src="https://cdn.shopify.com/s/files/1/0538/9280/8895/products/DM3384-600-7.png?v=1665052479"
-                alt="Product 7 of 7"
-              />
-            </li>
-          </ul>
           <div
             className="splide wishlist__imageFull"
             role="region"
@@ -232,12 +167,17 @@ const WishlistSelectedDetail = () => {
           </span>
           <div className="wishlist__selectedProductDetail--size">
             <ArrowDown />
-            <label htmlFor="size">Footware</label>
+            <label htmlFor="size">Size</label>
             <select name="size" id="size">
               <option value="3">3</option>
               <option value="4">4</option>
               <option value="5">5</option>
               <option value="6">6</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
             </select>
           </div>
           <div className="wishlist__action">
