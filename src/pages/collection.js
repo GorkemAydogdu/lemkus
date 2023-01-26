4d1bfc7cd0ba8fbb4b4e12715ea895816cb5d391
Görkem Aydoğdu <gorkem_aydogdu15@hotmail.com>
2023-01-26 18:53:10 +0300

    Lemkus Clone

--- src/pages/collection.js
+++ src/pages/collection.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 
 // import SmoothScrollWrapper from "../components/UI/SmoothScrollWrapper";
 // import Footer from "../components/Footer/Footer";
