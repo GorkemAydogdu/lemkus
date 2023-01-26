447cc07c53f1c72d203349abcdbe0a6fc76948e1
Görkem Aydoğdu <gorkem_aydogdu15@hotmail.com>
2023-01-26 18:51:05 +0300

    Lemkus Clone

--- src/pages/collection.js
+++ src/pages/collection.js
@@ -9,52 +9,53 @@ import { ReactComponent as SearchIcon } from "../assets/search.svg";
 const Collection = () => {
   const smoothScrollWrapper = useRef();
   return (
-    <SmoothScrollWrapper ref={smoothScrollWrapper} className="pageSmooth">
-      <div className="collection">
-        <div className="collection__header">
-          <h1 className="collection__title">Sneakers</h1>
-          <div className="collection__searchBox">
-            <div className="collection__input">
-              <input type="text" placeholder="Search Products" />
-            </div>
-            <Button className="collection__searchButton">
-              <SearchIcon />
-            </Button>
-          </div>
-          <div className="collection__action">
-            <Button className="collection__hide">Hide Filter</Button>
-            <div className="collection__filter">
-              <span className="collection__totalProducts">
-                <span>311</span> Products
-              </span>
-              <div className="collection__showSize">
-                <label htmlFor="setLimit">Show</label>
-                <select name="setLimit" id="setLimit">
-                  <option value="12">12</option>
-                  <option value="24">24</option>
-                  <option value="48">48</option>
-                </select>
-              </div>
-              <div className="collection__sortBy">
-                <label htmlFor="sortBy">Date,new to old</label>
-                <div className="collection__sortOption">
-                  <span>Best Selling</span>
-                  <span>Alphabetically, A-Z</span>
-                  <span>Alphabetically, Z-A</span>
-                  <span>Price,low to high</span>
-                  <span>Price,high to low</span>
-                  <span>Date,new to old</span>
-                  <span>Date,old to new</span>
-                  <span>% Sale off</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="collection__content"></div>
-      </div>
-      <Footer />
-    </SmoothScrollWrapper>
+    <></>
+    // <SmoothScrollWrapper ref={smoothScrollWrapper} className="pageSmooth">
+    //   <div className="collection">
+    //     <div className="collection__header">
+    //       <h1 className="collection__title">Sneakers</h1>
+    //       <div className="collection__searchBox">
+    //         <div className="collection__input">
+    //           <input type="text" placeholder="Search Products" />
+    //         </div>
+    //         <Button className="collection__searchButton">
+    //           <SearchIcon />
+    //         </Button>
+    //       </div>
+    //       <div className="collection__action">
+    //         <Button className="collection__hide">Hide Filter</Button>
+    //         <div className="collection__filter">
+    //           <span className="collection__totalProducts">
+    //             <span>311</span> Products
+    //           </span>
+    //           <div className="collection__showSize">
+    //             <label htmlFor="setLimit">Show</label>
+    //             <select name="setLimit" id="setLimit">
+    //               <option value="12">12</option>
+    //               <option value="24">24</option>
+    //               <option value="48">48</option>
+    //             </select>
+    //           </div>
+    //           <div className="collection__sortBy">
+    //             <label htmlFor="sortBy">Date,new to old</label>
+    //             <div className="collection__sortOption">
+    //               <span>Best Selling</span>
+    //               <span>Alphabetically, A-Z</span>
+    //               <span>Alphabetically, Z-A</span>
+    //               <span>Price,low to high</span>
+    //               <span>Price,high to low</span>
+    //               <span>Date,new to old</span>
+    //               <span>Date,old to new</span>
+    //               <span>% Sale off</span>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div className="collection__content"></div>
+    //   </div>
+    //   <Footer />
+    // </SmoothScrollWrapper>
   );
 };
 
