bdee869db979a64bcfe2c5fa3e463658576ac2ab
Görkem Aydoğdu <gorkem_aydogdu15@hotmail.com>
2023-02-07 18:25:38 +0300

    Lemkus Clone

---
+++ src/pages/collectionDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CollectionDetail = () => {
+  return (
+    <div className="collectionDetail">
+      <div className="collectionDetail__images"></div>
+      <div className="collectionDetail__infos"></div>
+    </div>
+  );
+};
+
+export default CollectionDetail;
