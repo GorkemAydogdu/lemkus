952ae40dadfe07742428554687c0623a095a910b
Görkem Aydoğdu <gorkem_aydogdu15@hotmail.com>
2023-03-31 15:55:48 +0300

    Lemkus Clone

--- frontend/src/pages/collectionDetail.js
+++ frontend/src/pages/collectionDetail.js
@@ -134,6 +134,7 @@ const CollectionDetail = (props) => {
       );
       clickedData[0].images.forEach((image, idx) => {
         thumbnailButton[idx].innerHTML = `<img src=${image.url} alt=''/>`;
+        console.log("ZORT");
       });
     }
   }, [clickedData, clickedSize]);
