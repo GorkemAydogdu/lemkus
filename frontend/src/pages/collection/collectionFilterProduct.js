b1a4cbeb1a1de7b78c28b6427ad273a4fe370945
Görkem Aydoğdu <gorkem_aydogdu15@hotmail.com>
2023-04-16 17:33:29 +0300

    Lemkus Clone

--- frontend/src/pages/collection/collectionFilterProduct.js
+++ frontend/src/pages/collection/collectionFilterProduct.js
@@ -1,18 +1,24 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
+import React from "react";
+import { useNavigate, useLocation, useSearchParams } from "react-router-dom";
+import Button from "../../components/UI/Button";
 import { ReactComponent as Arrow } from "../../assets/chevron-right.svg";
+import { ReactComponent as X } from "../../assets/x.svg";
 const CollectionFilterProduct = ({ filterBrand, filterType, filterGender }) => {
-  const [brandIsActive, setBrandIsActive] = useState(false);
-  const [typeIsActive, setTypeIsActive] = useState(false);
-  const [genderIsActive, setGenderIsActive] = useState(false);
-
   function filterData(data) {
     return Array.from(
       data.reduce((r, c) => r.set(c, (r.get(c) || 0) + 1), new Map()),
       ([key, count]) => ({ key, count })
     );
   }
+
+  function useQuery() {
+    const { search } = useLocation();
+    return React.useMemo(() => new URLSearchParams(search), [search]);
+  }
+  let query = useQuery();
+  const brand = query.get("brand");
+  const gender = query.get("gender");
+  const category = query.get("category");
 
   const brandCount = filterData(filterBrand);
   const typeCount = filterData(filterType);
@@ -31,9 +37,76 @@ const CollectionFilterProduct = ({ filterBrand, filterType, filterGender }) => {
       search: searchParams.toString(),
     });
   };
-
+  const [searchParams, setSearchParams] = useSearchParams();
   return (
     <div className="collection__filterProduct">
+      {(searchParams.has("brand") ||
+        searchParams.has("category") ||
+        searchParams.has("gender")) && (
+        <div className="collection__filteredList">
+          <div className="collection__filteredHeader">
+            <h3>Filter By</h3>
+            <Button
+              onClick={() => {
+                if (
+                  searchParams.has("brand") ||
+                  searchParams.has("gender") ||
+                  searchParams.has("category")
+                ) {
+                  searchParams.delete("brand");
+                  searchParams.delete("gender");
+                  searchParams.delete("category");
+                  setSearchParams(searchParams);
+                }
+              }}
+              className="collection__clearAll underline"
+            >
+              Clear All
+            </Button>
+          </div>
+          <div className="collection__filteredGroup">
+            {brand && (
+              <Button
+                onClick={() => {
+                  if (searchParams.has("brand")) {
+                    searchParams.delete("brand");
+                    setSearchParams(searchParams);
+                  }
+                }}
+                className="collection__filteredButton"
+              >
+                <span>{brand.replaceAll("-", " ")}</span> <X />
+              </Button>
+            )}
+            {gender && (
+              <Button
+                onClick={() => {
+                  if (searchParams.has("gender")) {
+                    searchParams.delete("gender");
+                    setSearchParams(searchParams);
+                  }
+                }}
+                className="collection__filteredButton"
+              >
+                <span>{gender.replaceAll("-", " ")}</span> <X />
+              </Button>
+            )}
+            {category && (
+              <Button
+                onClick={() => {
+                  if (searchParams.has("category")) {
+                    searchParams.delete("category");
+                    setSearchParams(searchParams);
+                  }
+                }}
+                className="collection__filteredButton"
+              >
+                <span>{category.replaceAll("-", " ")}</span> <X />
+              </Button>
+            )}
+          </div>
+        </div>
+      )}
       <div className="collection__filterProduct--item">
         <span className="collection__filterProduct--title">
           Brand
@@ -44,7 +117,6 @@ const CollectionFilterProduct = ({ filterBrand, filterType, filterGender }) => {
           {brandCount.map((filter) => (
             <li
               onClick={() => {
-                setBrandIsActive((prevState) => !prevState);
                 addQuery(
                   "brand",
                   filter.key.toLowerCase().replaceAll(" ", "-")
@@ -52,11 +124,12 @@ const CollectionFilterProduct = ({ filterBrand, filterType, filterGender }) => {
               }}
               key={Math.random()}
               className={`collection__item ${
-                brandIsActive ? "collection__item--active" : ""
+                brand === filter.key.toLowerCase().replaceAll(" ", "-") &&
+                "collection__item--active"
               }`}
             >
               <span className="collection__item--checkbox"></span>
-              <span className="collection__item--brand">{filter.key}</span>
+              <span className="collection__item--name">{filter.key}</span>
               <span className="collection__item--count">({filter.count})</span>
             </li>
           ))}
@@ -74,7 +147,6 @@ const CollectionFilterProduct = ({ filterBrand, filterType, filterGender }) => {
               return (
                 <li
                   onClick={() => {
-                    setTypeIsActive((prevState) => !prevState);
                     addQuery(
                       "category",
                       filter.key.toLowerCase().replaceAll(" ", "-")
@@ -82,11 +154,13 @@ const CollectionFilterProduct = ({ filterBrand, filterType, filterGender }) => {
                   }}
                   key={Math.random()}
                   className={`collection__item ${
-                    typeIsActive ? "collection__item--active" : ""
+                    category ===
+                      filter.key.toLowerCase().replaceAll(" ", "-") &&
+                    "collection__item--active"
                   }`}
                 >
                   <span className="collection__item--checkbox"></span>
-                  <span className="collection__item--brand">{filter.key}</span>
+                  <span className="collection__item--name">{filter.key}</span>
                   <span className="collection__item--count">
                     ({filter.count})
                   </span>
@@ -106,7 +180,6 @@ const CollectionFilterProduct = ({ filterBrand, filterType, filterGender }) => {
           {genderCount.map((filter) => (
             <li
               onClick={() => {
-                setGenderIsActive((prevState) => !prevState);
                 addQuery(
                   "gender",
                   filter.key.toLowerCase().replaceAll(" ", "-")
@@ -114,11 +187,12 @@ const CollectionFilterProduct = ({ filterBrand, filterType, filterGender }) => {
               }}
               key={Math.random()}
               className={`collection__item ${
-                genderIsActive ? "collection__item--active" : ""
+                gender === filter.key.toLowerCase().replaceAll(" ", "-") &&
+                "collection__item--active"
               }`}
             >
               <span className="collection__item--checkbox"></span>
-              <span className="collection__item--brand">{filter.key}</span>
+              <span className="collection__item--name">{filter.key}</span>
               <span className="collection__item--count">({filter.count})</span>
             </li>
           ))}
