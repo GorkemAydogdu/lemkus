493f9293371ac0ebd33cb9a61264fbb0738afb93
Görkem Aydoğdu <gorkem_aydogdu15@hotmail.com>
2023-04-24 20:08:13 +0300

    Lemkus Clone

--- frontend/src/components/Banner/Banner.js
+++ frontend/src/components/Banner/Banner.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
   return (
     <div className="banner">
-      <a href="/" className="banner__link">
+      <Link to={``} className="banner__link">
         <img
           src="https://cdn.shopify.com/s/files/1/0538/9280/8895/files/2002R_Lemkus_Hero_Image_2636x1092_f4e658d1-9bf6-451d-a7c0-4a77c6f082b5.jpg?v=1681889396"
           className="banner__image"
-          alt="Nike Air Trainer"
+          alt="New Balance 2002R"
         />
-      </a>
+      </Link>
     </div>
   );
 };
