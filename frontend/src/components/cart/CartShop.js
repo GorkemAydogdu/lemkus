0fe626981f938ab0f9e3ce254c600fe1b403d7ad
Görkem Aydoğdu <gorkem_aydogdu15@hotmail.com>
2023-04-26 20:47:49 +0300

    Lemkus Clone

--- frontend/src/components/cart/CartShop.js
+++ frontend/src/components/cart/CartShop.js
@@ -1,34 +1,56 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
-// import CartShopItem from "./CartShopItem";
+import CartShopItem from "./CartShopItem";
 
 //Splidejs
 import Splide from "@splidejs/splide";
 
 import "@splidejs/splide/css";
 
-const CartShop = (props) => {
-  useEffect(() => {
-    let splide = new Splide(".cart__shop--group", {
-      height: "auto",
-      direction: "ttb",
-      drag: false,
-      speed: 800,
-      arrows: false,
-      pagination: false,
-      breakpoints: {
-        1024: {
-          drag: "free",
-          direction: "ltr",
-          perPage: 2,
-        },
-        500: {
-          perPage: 1,
-        },
-      },
-    });
+const CartShop = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const fetchDataHandler = useCallback(async () => {
+    try {
+      setLoading(true);
+      const res = await fetch("http://localhost:5000/product");
+      const data = await res.json();
+      setData(
+        data.filter((filtered) => filtered.categoryName === "Accessories")
+      );
+      setLoading(false);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
 
-    splide.mount();
+  useEffect(() => {
+    fetchDataHandler();
+  }, [fetchDataHandler]);
+
+  useEffect(() => {
+    if (loading !== true) {
+      let splide = new Splide(".cart__shop--group", {
+        height: "auto",
+        direction: "ttb",
+        drag: false,
+        speed: 800,
+        arrows: false,
+        pagination: false,
+        breakpoints: {
+          1024: {
+            drag: "free",
+            direction: "ltr",
+            perPage: 2,
+          },
+          500: {
+            perPage: 1,
+          },
+        },
+      });
+
+      splide.mount();
+    }
   }, []);
   return (
     <div className="cart__shop">
@@ -36,15 +58,16 @@ const CartShop = (props) => {
       <div className="splide cart__shop--group">
         <div className="splide__track">
           <ul className="splide__list cart__shop--list">
-            {/* {props.data.map((item) => (
-              <CartShopItem
-                key={item.id}
-                image={item.image}
-                logo={item.logo}
-                name={item.name}
-                price={item.price}
-              />
-            ))} */}
+            {data !== [] &&
+              data.map((item) => (
+                <CartShopItem
+                  key={item._id}
+                  image={item.image}
+                  logo={item.logo}
+                  name={item.name}
+                  price={item.price}
+                />
+              ))}
           </ul>
         </div>
       </div>
