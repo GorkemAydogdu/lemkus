cea450e0c051a23cc49a8d8ecbdd1e9344368f54
Görkem Aydoğdu <gorkem_aydogdu15@hotmail.com>
2023-04-26 17:11:20 +0300

    Lemkus Clone

--- frontend/src/components/Banner/Banner.js
+++ frontend/src/components/Banner/Banner.js
@@ -4,7 +4,10 @@ import { Link } from "react-router-dom";
 const Banner = () => {
   return (
     <div className="banner">
-      <Link to={``} className="banner__link">
+      <Link
+        to={`/products/2002r-lifestyle?id=643fe0d7ddffb915f649f7d2`}
+        className="banner__link"
+      >
         <img
           src="https://cdn.shopify.com/s/files/1/0538/9280/8895/files/2002R_Lemkus_Hero_Image_2636x1092_f4e658d1-9bf6-451d-a7c0-4a77c6f082b5.jpg?v=1681889396"
           className="banner__image"
