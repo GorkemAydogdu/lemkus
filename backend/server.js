00617d5fd7acd46a0664ade5cb19a8ae201ba43f
Görkem Aydoğdu <gorkem_aydogdu15@hotmail.com>
2023-04-28 19:11:49 +0300

    Lemkus Clone

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,15 @@ require("dotenv").config({ path: "./.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/product"));
-app.use(require("./routes/news"));
-app.use(require("./routes/menu"));
-app.use(require("./routes/brands"));
-app.use(require("./routes/collections"));
-app.use(require("./routes/wishlist"));
-app.use(require("./routes/orders"));
+app.use(require("./api/routes/product"));
+app.use(require("./api/routes/news"));
+app.use(require("./api/routes/menu"));
+app.use(require("./api/routes/brands"));
+app.use(require("./api/routes/collections"));
+app.use(require("./api/routes/wishlist"));
+app.use(require("./api/routes/orders"));
 // get driver connection
-const dbo = require("./db/conn");
+const dbo = require("./api/db/conn");
 
 //https://www.mongodb.com/community/forums/t/solved-mern-tutorial-issue-cannot-read-properties-of-undefined-reading-collection/212521
 app.listen(port, async () => {
